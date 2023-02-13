db42fbdc08e685e2c2f4253f082ef1989fba2a14
Andry Salazar <avesalazar4@gmail.com>
2023-02-13 17:27:56 -0500

    feat: delete post

--- src/lib/firebase-app.js
+++ src/lib/firebase-app.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, deleteDoc, doc } from 'firebase/firestore';
 
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -22,3 +22,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+export const deletePost = (id) => deleteDoc(doc(db, 'post', id));
